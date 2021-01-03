055d840b3d234eacc4b2279b6747662d0c3a8663
mafiapp <76881300+mafiapp@users.noreply.github.com>
2021-01-03 12:01:41 +0600

    Add files via upload

---
+++ precache-manifest.0852b5502573b80f159fc7d60a139276.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "e5571d6c42ea1b30e2b7",
+    "url": "/static/css/main.4765d134.chunk.css"
+  },
+  {
+    "revision": "e5571d6c42ea1b30e2b7",
+    "url": "/static/js/main.22e354cb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "03d9658cb9a6ba95982a",
+    "url": "/static/css/2.c65b1102.chunk.css"
+  },
+  {
+    "revision": "03d9658cb9a6ba95982a",
+    "url": "/static/js/2.c3a4b0b1.chunk.js"
+  },
+  {
+    "revision": "2d23aa6c037b4e50144256d34ff33007",
+    "url": "/static/media/music.2d23aa6c.mp3"
+  },
+  {
+    "revision": "53421381b89352705019c0a865c77508",
+    "url": "/static/media/night.53421381.mp3"
+  },
+  {
+    "revision": "448dbb5b16e6caf610008c8fff059bfd",
+    "url": "/static/media/newmsg.448dbb5b.mp3"
+  },
+  {
+    "revision": "62ba49dff8d6107acf14b20a1605fcdb",
+    "url": "/static/media/day.62ba49df.mp3"
+  },
+  {
+    "revision": "700de84941d0efdb2bc34ed07f117659",
+    "url": "/static/media/gameRes.700de849.mp3"
+  },
+  {
+    "revision": "59dcdff3ca60931fb5de842cd8587acd",
+    "url": "/static/media/night.59dcdff3.png"
+  },
+  {
+    "revision": "1ad55a21de30da257971d2e33f3a0e14",
+    "url": "/index.html"
+  }
+];
